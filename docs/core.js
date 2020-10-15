e07e36f48ac28e69e687e5f4f31c69f048f0b482
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-15 22:27:31 +0200

    Update

--- docs/core.js
+++ docs/core.js
@@ -1,7 +1,7 @@
 import { node, fetchAPI } from 'https://unpkg.com/cutleryjs@3.5.5/dist/js/index.js';
 import 'https://unpkg.com/feather-icons@4.28.0/dist/feather.min.js';
 import { mapboxInit } from './static/modules/mapbox.js';
-import {pathCallback} from '../static/modules/routing.js'
+import {pathCallback} from './static/modules/routing.js'
 import {sesamCollapse} from 'https://unpkg.com/sesam-collapse';
 
 pathCallback('/')((path) => {
